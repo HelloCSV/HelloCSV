8b769d941f9835f2f7fbc64da415f1b1c44c6333
Emily Fields <80897288+emily-fields@users.noreply.github.com>
2025-03-29 23:22:51 -0700

    add gray sections

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -14,25 +14,27 @@ const App = () => {
     <div className="min-h-screen w-full">
       <Header />
 
-      <div className="content">
-        <div className="documentation-container">
-          <h3 className="font-title">
-            Installation
-          </h3>
-          <p>
-            Works with any Javascript application, <span className="font-semibold italic">even if you don't use React.</span>.
-          </p>
+      <div className="section">
+        <div className="content">
+          <div className="documentation-container">
+            <h3 className="font-title">Installation</h3>
+            <p>
+              Works with any Javascript application,{' '}
+              <span className="font-semibold italic">
+                even if you don't use React.
+              </span>
+              .
+            </p>
+          </div>
+          <InstallTabs />
         </div>
-        <InstallTabs />
       </div>
 
-      <div className="mb-16">
+      <div className="section bg-gray-100">
         <div className="content">
           <a id="introduction"></a>
           <div className="documentation-container">
-            <h3 className="font-title">
-              Building a CSV uploader is hard
-            </h3>
+            <h3 className="font-title">Building a CSV uploader is hard</h3>
             <p className="text-lg">
               HelloCSV is a javascript library that makes it easy to drop in a
               powerful, intuitive, and elegant CSV uploader that works with any
@@ -40,11 +42,16 @@ const App = () => {
             </p>
             <br />
             <p>
-              The best part? It's <span className="underline decoration-blue-500 underline-offset-6 decoration-4">free and open source.</span>
+              The best part? It's{' '}
+              <span className="underline decoration-blue-500 decoration-4 underline-offset-6">
+                free and open source.
+              </span>
             </p>
           </div>
         </div>
+      </div>
 
+      <div className="section">
         <div className="content">
           <a id="getting-started"></a>
           <div className="documentation-container">
@@ -52,14 +59,20 @@ const App = () => {
               Drop in an uploader into your app in seconds
             </h3>
             <p>
-              A simple, yet powerful Javascript API to fit your needs. Transform, validate, clean, and slice your data.
+              A simple, yet powerful Javascript API to fit your needs.
+              Transform, validate, clean, and slice your data.
             </p>
           </div>
 
           <CodeBlock code={EXAMPLE_CODE} />
         </div>
+      </div>
 
+      <div className="section bg-gray-100">
         <EmployeeImporter />
+      </div>
+
+      <div className="section mb-16">
         <EmployeeSheetImporter />
       </div>
 
