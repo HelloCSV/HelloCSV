f5b842c2321358e0ba86790011302c1407b5e46b
rvhoyt <rvhoyt@gmail.com>
2025-11-03 12:09:07 -0500

    fix lint

--- src/validators/validator_definitions/unique_validator.ts
+++ src/validators/validator_definitions/unique_validator.ts
@@ -4,6 +4,7 @@ import { Validator } from './base';
 
 export class UniqueValidator extends Validator {
   seen: Set<ImporterOutputFieldType>;
+
   definition: UniqueValidatorDefinition;
 
   constructor(definition: UniqueValidatorDefinition) {
@@ -13,10 +14,7 @@ export class UniqueValidator extends Validator {
   }
 
   private comparableValue(fieldValue: ImporterOutputFieldType) {
-    if (
-      this.definition.caseInsensitive &&
-      typeof fieldValue === 'string'
-    ) {
+    if (this.definition.caseInsensitive && typeof fieldValue === 'string') {
       return fieldValue.toLocaleLowerCase();
     }
     return fieldValue;
