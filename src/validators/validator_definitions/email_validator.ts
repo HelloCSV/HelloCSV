2cad15f1c8036e1d17e3f7a39501c0d696a06069
rvhoyt <rvhoyt@gmail.com>
2025-11-17 11:45:16 -0500

    remove duplicate

--- src/validators/validator_definitions/email_validator.ts
+++ src/validators/validator_definitions/email_validator.ts
@@ -7,7 +7,6 @@ export class EmailValidator extends RegexValidator {
       ...definition,
       regex:
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      validate: 'email',
     });
   }
 }
